042f983954125078daf8012d2c41c75d0bbacee6
Mathias Bylund <materbyl@gmail.com>
2016-12-03 00:36:56 +0100

    Update
    - Changed the css and template for the blackboard to better support several nodes
    - Fixed an incorrect identation in httprequest.repy when parsing parameters

--- src/make_frames.sh
+++ src/make_frames.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo "<html><body>"
+echo "<html><head><style>
+        iframe {
+            border: solid black 1px;
+        }
+        </style></head><body>"
 for y in `cat neighborlist.txt` 
 do 
 echo "<iframe src=\"http://$y\" width="48%" height=300></iframe>"
