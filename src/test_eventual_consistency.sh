82053350ce15fa4f634f1202ba228e7be678affd
Mathias Bylund <materbyl@gmail.com>
2016-12-13 13:17:37 +0100

    Update
    - Changed vectorClock back to logicalClock to implement a more easy ordering
    - Added a unique id due to the fact that different clients would generate the same id. It is not truly unqiue though but should work in the context.

--- src/test_eventual_consistency.sh
+++ src/test_eventual_consistency.sh
@@ -1,4 +1,5 @@
 for i in `seq 1 250`; 
     do 
         curl -d 'entry=t'${i} -X 'POST' 'http://127.0.0.1:63001/entries'
+        curl -d 'entry=e'${i} -X 'POST' 'http://127.0.0.1:63002/entries'
 done
